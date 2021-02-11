cf02648c0bd7eea955875916b019dec16a8504a3
lerttosprorn <lerttosporn.s@ku.th>
2021-02-11 17:59:35 +0700

    fristday

---
+++ 6320503084_2_5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+ int d,m,c;
+    scanf("%d %d",&d,&m);
+    if(d>7)
+    {
+     d=d%7;
+    }
+ switch(m)
+    {
+        case 1: n=31; break;
+        case 2: n=28; break;
+        case 3: n=31; break;
+        case 4: n=30; break;
+        case 5: n=31; break;
+        case 6: n=30; break;
+        case 7: n=31; break;
+        case 8: n=31; break;
+        case 9: n=30; break;
+        case 10: n=31; break;
+        case 11: n=30; break;
+        case 12: n=31; break;
+    }
+    if((n+a)%7==0)
+    {
+ printf("7");
+    }
+    else
+ printf("%d",(c+a)%7);
+}
